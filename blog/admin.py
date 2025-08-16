4cde9b9cc3cd71624691cc3c6963ceb4f8d6529a
Ashkan <ashkan_ahrari@yahoo.com>
2025-08-17 00:29:52 +0300

    add periodictask to admin panel

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.utils.html import format_html
 from mptt.admin import MPTTModelAdmin
+from django_celery_beat.models import PeriodicTask, IntervalSchedule
 
 from .models import Post, Comment, Tag, Media, Notification
 from pages.models import FeedBack
@@ -64,3 +65,5 @@ class FeedbackAdmin(admin.ModelAdmin):
 admin.site.register(Notification)
 admin.site.register(Tag)
 admin.site.register(Media)
+admin.site.register(IntervalSchedule)
+admin.site.register(PeriodicTask)
