33815a4ffe5430dbf8e833e5b6b339ff1540162a
Ashkan <ashkan_ahrari@yahoo.com>
2026-01-06 15:59:20 +0300

    Fix the postgres host problem

--- config/settings.py
+++ config/settings.py
@@ -144,7 +144,7 @@ else:
             "NAME": config("DB_NAME"),
             "USER": config("DB_USER"),
             "PASSWORD": config("DB_PASSWORD"),
-            "HOST": "localhost",
+            'HOST': config('DB_HOST'),
             "PORT": "5432",
         }
     }
